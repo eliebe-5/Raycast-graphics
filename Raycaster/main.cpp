76ea1c6cb7522af6761b87589792af55d344a2d0
Elias Eberhardsson <Elias.Eberhardsson@dataductus.se>
2019-01-09 15:56:17 +0100

    Threading

--- Raycaster/main.cpp
+++ Raycaster/main.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 #include <float.h>
 #include <random>
+#include <thread>
+#include <vector>
 #include "material.h"
 #include "camera.h"
 #include "sphere.h"
 #include "hitable_list.h"
 #include <SDL.h>
+#include <Windows.h>
 
 math::vec color(raycaster::ray& r, hitable *world, int depth) {
 	hit_record rec;
@@ -59,10 +62,18 @@ hitable *random_scene(int balls) {
 				math::vec center(a, b, c);
 				if (b % 3 == 0)
 				{
-					list[i++] = new sphere(center, 0.4f,
-						//new metal(math::vec(0.5f, 0.5f, 0.5f), 0.1));
-						//new dielectric(1.5));
-						new lambertian(math::vec(0, 0, 0)));
+					if (c % 2 == 0)
+					{
+						list[i++] = new sphere(center, 0.4f,
+							new metal(math::vec(0.5f, 0.5f, 0.5f), 0.1));
+
+					}
+					else {
+						list[i++] = new sphere(center, 0.4f,
+							//new metal(math::vec(0.5f, 0.5f, 0.5f), 0.1));
+							//new dielectric(1.5));
+							new lambertian(math::vec(0, 0, 0)));
+					}
 				}
 				else if (b % 3 == 1 || b % 3 == -2)
 				{
@@ -107,44 +118,92 @@ inline void set_pixel(int x, int y, int ns, int nx, int ny, camera cam, hitable*
 	b[x + nx*y] = col[2];
 }
 
+SDL_Window *window; 
+
+void set_pixels(int from_x, int to_x, int from_y, int to_y, int ns, int nx, int ny, camera cam, hitable* world, unsigned char* r, unsigned char* g, unsigned char* b, unsigned char* pixels)
+{
+	if (from_y > to_y) {
+		for (int j = from_y; j >= to_y; j--) {
+			if (from_x < to_x)
+			{
+				for (int i = from_x; i < to_x; i++) {
+					set_pixel(i, j, ns, nx, ny, cam, world, r, g, b);
+					pixels[i * 3 + j * 3 * nx] = r[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 1] = g[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 2] = b[i + j * nx];
+				}
+			}
+			else 
+			{
+				for (int i = from_x; i >= to_x; i--) {
+					set_pixel(i, j, ns, nx, ny, cam, world, r, g, b);
+					pixels[i * 3 + j * 3 * nx] = r[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 1] = g[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 2] = b[i + j * nx];
+				}
+			}
+		}
+	}
+	else {
+		for (int j = from_y; j <= to_y; j++) {
+			if (from_x < to_x)
+			{
+				for (int i = from_x; i < to_x; i++) {
+					set_pixel(i, j, ns, nx, ny, cam, world, r, g, b);
+					pixels[i * 3 + j * 3 * nx] = r[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 1] = g[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 2] = b[i + j * nx];
+				}
+			}
+			else
+			{
+				for (int i = from_x; i >= to_x; i--) {
+					set_pixel(i, j, ns, nx, ny, cam, world, r, g, b);
+					pixels[i * 3 + j * 3 * nx] = r[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 1] = g[i + j * nx];
+					pixels[i * 3 + j * 3 * nx + 2] = b[i + j * nx];
+				}
+			}
+		}
+	}
+}
+
 int main(int argc, char** argv)
 {
+	int nx = 400;
+	int ny = 300;
+	int ns = 200;
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
-
-	SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, 800, 600,
-		SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("Hello World!", 100, 100, nx, ny, SDL_WINDOW_SHOWN);
 	if (window == NULL) {
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 
-	int nx = 800;
-	int ny = 600;
-	int ns = 200;
-	unsigned char* r = new unsigned char[800 * 600];
-	unsigned char* g = new unsigned char[800 * 600];
-	unsigned char* b = new unsigned char[800 * 600];
+	
+	unsigned char* r = new unsigned char[nx * ny];
+	unsigned char* g = new unsigned char[nx * ny];
+	unsigned char* b = new unsigned char[nx * ny];
 	hitable **list;
 	hitable *world;
 	world = random_scene(6);
 	
-	unsigned char* pixels = new unsigned char[800 * 600 * 3];
+	unsigned char* pixels = new unsigned char[nx * ny * 3];
 
 	SDL_Surface *surface_bmp = SDL_CreateRGBSurfaceFrom((void*)pixels,
-		800,
-		600,
-		3 * 8,          // bits per pixel = 24
-		800 * 3,  // pitch
-		0x0000FF,              // red mask
-		0x00FF00,              // green mask
-		0xFF0000,              // blue mask
-		0);                    // alpha mask (none)
+		nx,
+		ny,
+		3 * 8,					// bits per pixel = 24
+		nx * 3,				// pitch
+		0x0000FF,				// red mask
+		0x00FF00,				// green mask
+		0xFF0000,				// blue mask
+		0);						// alpha mask (none)
 
 	SDL_Surface *surface_window = SDL_GetWindowSurface(window);
-
 
 	float x = 13.0f;
 	while (true)
@@ -154,24 +213,77 @@ int main(int argc, char** argv)
 		float dist_to_focus = (lookfrom - lookat).mag();
 		float aperture = 0.1f;
 		camera cam(lookfrom, lookat, math::vec(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
-		for (int j = ny - 1; j >= 0; j--) {
-			for (int i = 0; i < nx; i++) {
-				set_pixel(i, j, ns, nx, ny, cam, world, r, g, b);
-			}
-		}
-		int j = 800 * 600;
-		for (int i = 0; i < 800 * 600 * 3; i += 3) {
-			pixels[i] = r[j];
-			pixels[i + 1] = g[j];
-			pixels[i + 2] = b[j];
-			j--;
-		}
+		
 		SDL_BlitSurface(surface_bmp,
 			NULL,
 			surface_window,
 			NULL);
 		SDL_UpdateWindowSurface(window);
-		//x += 5.f;
+
+		int start_x = 0;
+		int start_y = 0;
+		int interval_x = 2;
+		int interval_y = 10;
+
+		std::thread rendering([](SDL_Surface *a, SDL_Surface *b, SDL_Window *c) {
+			for (;;) {  
+				SDL_Event event;
+				while (SDL_PollEvent(&event)) {
+					switch (event.type)
+					{
+					case SDL_KEYDOWN:
+						if (event.key.keysym.sym == SDLK_ESCAPE)
+							return;
+						break;
+					case SDL_QUIT:
+						return;
+						break;
+					}
+				}
+
+				Sleep(1000.f / 60.f);
+				SDL_BlitSurface(a,
+					NULL,
+					b,
+					NULL);
+				SDL_UpdateWindowSurface(c);
+			}
+		}, surface_bmp, surface_window, window);
+
+		std::vector<int*> xys;
+		std::vector<std::thread> threads;
+		for (;;)
+		{
+			for (;;)
+			{
+				if (rand() % 2)
+					xys.push_back(new int[4]{ start_x, start_x + interval_x, start_y, start_y + interval_y });
+				else
+					xys.push_back(new int[4]{ start_x + interval_x, start_x, start_y + interval_y, start_y });
+
+				start_y += interval_y;
+				if (start_y == ny - 1)
+					break;
+				if (start_y + interval_y >= ny)
+					start_y = ny - interval_y - 1;
+			}
+			start_x += interval_x;
+			if (start_x == nx - 1)
+				break;
+			if (start_x + interval_x >= nx)
+				start_x = nx - interval_x - 1;
+			start_y = 0;
+		}
+
+		for (int i = xys.size() - 1; i >= 0; i--)
+		{
+			int ind = rand() % xys.size();
+			threads.push_back(std::thread(set_pixels, xys[ind][0], xys[ind][1], xys[ind][2], xys[ind][3], ns, nx, ny, cam, world, r, g, b, pixels));
+			xys.erase(xys.begin() + ind);
+		}
+		
+		rendering.join();
+		
 	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
